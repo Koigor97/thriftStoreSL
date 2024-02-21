ce587c349185bc9a8045ecbf478a02fcbdef6b63
Koigor97 <turaysamuel101@yahoo.com>
2024-02-21 22:01:41 +0200

    create and add orderModel.js file

---
+++ backend/models/orderModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+  orderItems: [
+    {
+      name: { type: String, required: true },
+      quantity: { type: Number, required: true },
+      image: { type: String, required: true },
+      price: { type: Number, required: true },
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Product",
+      },
+    },
+  ],
+});
