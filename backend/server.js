72bcfa3cd5da6375af825745d402f4ede41bb4cf
Koigor97 <turaysamuel101@yahoo.com>
2024-02-21 21:25:51 +0200

    initiate connection

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,10 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
+
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 8000;
